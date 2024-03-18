ffed183475a73c1693098ac24afb159486a5cfd0
SYGalvin <sygalvinchan@gmail.com>
2024-03-18 17:41:55 +0000

    update username and email

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import sys
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
 if (len(sys.argv) < 3):
     print("Not enough arguments")
     exit(1)
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 app = ApplicationBuilder().token(sys.argv[2]).build()
 
